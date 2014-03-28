cc321cfcf2663e2de0e8dfe42a57fbace3867f4a
Peter Indiola <pindiola@gmail.com>
2014-03-29 04:15:41 +0800

    lattice sample codes.

---
+++ lattice/get_stores.rb
@@ -0,0 +1,10 @@
+#! /usr/bin/env ruby
+require 'httparty'
+require 'json'
+require 'pp'
+
+user, pass = ENV['CC_API_KEY'].split(":")
+auth = {:username => user, :password => pass}
+response = HTTParty.get("https://api.crystalcommerce.com/v1/lattice/stores", :basic_auth => auth)
+parsed = JSON.parse(response.body)
+pp parsed
